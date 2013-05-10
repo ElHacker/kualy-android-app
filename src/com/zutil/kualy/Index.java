bba66372f5d7cd48f8a3810e21671088003afaa0
Braulio Chavez <pataslargas27@gmail.com>
2013-05-10 11:24:45 -0700

    Adds holoeverywhere library

--- src/com/zutil/kualy/Index.java
+++ src/com/zutil/kualy/Index.java
@@ -1,17 +1,16 @@
 package com.zutil.kualy;
 
-import com.zutil.lib.ImageHelper;
 
-import android.os.Bundle;
-import android.app.Activity;
+import org.holoeverywhere.app.Activity;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Shader.TileMode;
-import android.view.Menu;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.zutil.lib.ImageHelper;
 
 public class Index extends Activity {
 
@@ -19,6 +18,17 @@ public class Index extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
+		
+		// Create action bar
+	    ActionBar bar = getSupportActionBar();
+	    bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+	    bar.setDisplayShowTitleEnabled(true);
+	    bar.setTitle("Kualy");
+	    BitmapDrawable background = new BitmapDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.action_bar_background));
+	    background.setTileModeX(android.graphics.Shader.TileMode.REPEAT); 
+	    bar.setBackgroundDrawable(background);
+		
+		// Fill the profile picture
 		ImageView profilePic = (ImageView) findViewById(R.id.profile_image);
 		// Get the circled bitmap
 		Bitmap circleBitmap = ImageHelper.getCircledBitmap(getResources(), 50);
@@ -26,11 +36,5 @@ public class Index extends Activity {
 		profilePic.setImageBitmap(circleBitmap);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_index, menu);
-		return true;
-	}
 
 }
