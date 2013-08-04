6e5d2bbeb0ff784af41c29a8ee66b6bfb044caa7
Braulio Chavez <pataslargas27@gmail.com>
2013-08-03 20:12:05 -0700

    Changed action bar background yellow noise for a plain yellow background

--- src/com/zutil/kualy/Challenges.java
+++ src/com/zutil/kualy/Challenges.java
@@ -15,49 +15,48 @@ import android.view.ViewGroup;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.zutil.adapters.ChallengeListAdapter;
-import com.zutil.lib.BaseListElement;
 import com.zutil.lib.ChallengeListElement;
 
 public class Challenges extends Activity {
-	
+
 	private static final String TAG = "Challenges";
-	
+
 	private ListView listView;
-	
+
 	private List<ChallengeListElement> listElements;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_challenges);
-		
+
 		// Create action bar
 	    ActionBar bar = getSupportActionBar();
 	    bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
 	    bar.setDisplayShowTitleEnabled(true);
 	    bar.setTitle("Retos");
-	    BitmapDrawable background = new BitmapDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.action_bar_background));
-	    background.setTileModeX(android.graphics.Shader.TileMode.REPEAT); 
+	    BitmapDrawable background = new BitmapDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.action_bar_background_yellow));
+	    background.setTileModeX(android.graphics.Shader.TileMode.REPEAT);
 	    bar.setBackgroundDrawable(background);
-		
+
 		listView = (ListView) findViewById(android.R.id.list);
 		// Set the listview header
 		LayoutInflater inflater = getLayoutInflater();
 		View header = inflater.inflate(R.layout.challenge_list_header, (ViewGroup) findViewById(R.id.challenge_header_layout_root));
 		listView.addHeaderView(header);
-		
+
 		// Set up the list view items, based on a list of
 	 	// BaseListElement items
 	 	listElements = new ArrayList<ChallengeListElement>();
-	 	
+
 	 	int colors[] = { R.color.orange_palette, R.color.purple, R.color.blue, R.color.green_palette};
 	 	for(int i = 0; i < 9; i++) {
 	 		ChallengeListElement event = new ChallengeListElement(this, "" + (i + 1), "Ayuda a un extraño", "Humanidad", "12 Logros", colors[i%4]);
 	 		listElements.add(event);
 	 	}
-	 	
+
 	 	listView.setAdapter(new ChallengeListAdapter(this, android.R.id.list, listElements, R.layout.challenge_list_element));
-		
+
 	}
 
 }
--- src/com/zutil/kualy/Index.java
+++ src/com/zutil/kualy/Index.java
@@ -7,58 +7,50 @@ import java.util.List;
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.widget.ListView;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.zutil.adapters.ActionListAdapter;
-import com.zutil.lib.BaseListElement;
 import com.zutil.lib.ActionListElement;
-import com.zutil.lib.ImageHelper;
 
 public class Index extends Activity {
-	
+
 	private static final String TAG = "Index";
-	
+
 	private LinearLayout linearLayoutHeader;
 	private ListView listView;
-	
+
 	private List<ActionListElement> listElements;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
-		
+
 		// Create action bar
 	    ActionBar bar = getSupportActionBar();
-	    bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
-	    bar.setDisplayShowTitleEnabled(true);
-	    bar.setTitle("Kualy");
-	    BitmapDrawable background = new BitmapDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.action_bar_background));
-	    background.setTileModeX(android.graphics.Shader.TileMode.REPEAT); 
+	    bar.setDisplayShowTitleEnabled(false);
+	    bar.setIcon(R.drawable.ic_action_bar_logo);
+	    BitmapDrawable background = new BitmapDrawable(BitmapFactory.decodeResource(getResources(), R.drawable.action_bar_background_yellow));
+	    background.setTileModeX(android.graphics.Shader.TileMode.REPEAT);
 	    bar.setBackgroundDrawable(background);
-		
+
 		listView = (ListView) findViewById(android.R.id.list);
-		
+
 		// Set up the list view items, based on a list of
 	 	// BaseListElement items
 	 	listElements = new ArrayList<ActionListElement>();
-	 	
+
 	 	for(int i = 0; i < 3; i++) {
 	 		ActionListElement event = new ActionListElement(this, "HackerOfDreams", "Que agusto descansan en su nuevo hogar, los encontre abandonados en un bote de basura. Pero ya tienen donde relajarse y disfrutar del invierno. Son tres hermanos. Ya quiero lograr los caracteres creo que son demasiados no se si exageramos con tantos.", "250", "http://foo.bar.com");
 	 		listElements.add(event);
 	 	}
-	 	
+
 	 	listView.setAdapter(new ActionListAdapter(this, android.R.id.list, listElements, R.layout.action_list_item));
-		
+
 	}
 
 
